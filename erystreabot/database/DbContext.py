ea042e3b6a82087b477ba232ace8f23ef7909522
Jarosław Korbut <korbutf1.j@gmail.com>
2021-03-27 14:59:16 +0100

    peepoBless

--- erystreabot/database/DbContext.py
+++ erystreabot/database/DbContext.py
@@ -1,26 +1,22 @@
 import pymongo
 import os
 
+
 class DbContext:
-        
-    def __init__(self, conString: str = os.environ["connectionString"], dbName: str = os.environ["databaseName"]):
+
+    def __init__(self, conString: str, dbName: str):
         self.context = pymongo.MongoClient(conString)[dbName]
 
-    
     def getReplacer(self, collection: str, dbName: str, key: str):
         test = {"key": key}
         return self.context[collection].find_one(test)
 
-    
     def insertReplacer(self, collection: str, key: str, response: str):
         entity = {"key": key, "response": response}
         return self.context[collection].insert_one(entity)
 
+    def getAllReplacers(self, collection: str):
+        return self.context[collection].find()
 
     def __initDatabase(self, collection: str, entities):
-        self.context[collection]insert_many(entities)
-
-if(__name__ == "__main__"):
-    context = DbContext("mongodb://localhost:27017", "erystrea-bot")
-    
-    
+        self.context[collection].insert_many(entities)
